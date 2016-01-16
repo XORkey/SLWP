78091a8c3a609dc4daec6cfed576b051f25ed584
Timo <timo.ruiter@xs4all.nl>
2016-01-16 15:49:49 +0100

    rewrote Key() to give key 0 if host is empty and to return a true unsigned int; do not create an empty key ring file (it should contain a key 0)

--- slwp/addons/ff/TIMO/lib/keyring.js
+++ slwp/addons/ff/TIMO/lib/keyring.js
@@ -1,10 +1,19 @@
 function Key(host)
 {
-	var host_hash = require('lib/sha2.js').SHA256(host);
-	for (var i = 0; i < Z.length; i++)
-		if (Z[i][0] == host_hash)			// Found a host?
-			return Z[i][1];					// Return the key.
-	return null;
+	var key = null;
+	if (host.length == 0)
+		key = Z[0][1];
+	else
+	{
+		var host_hash = require('lib/sha2.js').SHA256(host);
+		for (var i = 0; i < Z.length; i++)
+			if (Z[i][0] == host_hash)			// Found a host?
+			{
+				key = Z[i][1];
+				break;
+			}
+	}
+	return parseInt(key) >>> 0;
 }
 
 function KeyRing(file)
@@ -12,20 +21,15 @@ function KeyRing(file)
 	var z = [];
 	var fileIO = require('sdk/io/file');
 	var key_ring_file = fileIO.open(file, 'r');
-	if (key_ring_file.closed)
-	{
-		key_ring_file = fileIO.open(file, 'w');
-											// Create empty key ring file.
-	}
-	else
+	if (!key_ring_file.closed)
 	{
 		var key_ring = key_ring_file.read();
 		var hash_and_key = key_ring.split("\n");
 		for (var i = 0; i < hash_and_key.length; i++)
 			if (hash_and_key[i].length > 0)
 				z[i] = hash_and_key[i].split("=");
+		key_ring_file.close();
 	}
-	key_ring_file.close();
 	return z;
 }
 
